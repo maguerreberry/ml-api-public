076a025c6f463c938893bb342fa37ba0d025d6bf
Matthew Aguerreberry <matthew.aguerreberry@rappi.com>
2020-11-29 17:45:05 -0300

    add flask request logging

--- api/app.py
+++ api/app.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 import settings
-from flask import Flask
+import json
+from flask import Flask, request
+from time import strftime
 from views import router
 
 app = Flask(__name__)
 app.register_blueprint(router)
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    print(json.dumps({
+        'timestamp': timestamp,
+        'method': request.method,
+        'endpoint': request.full_path,
+        'status_code': response.status_code,
+    }))
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=settings.API_DEBUG)
